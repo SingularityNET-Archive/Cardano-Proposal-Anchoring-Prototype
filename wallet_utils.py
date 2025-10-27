404db9d3cf0ec076b895755dd99f9bf3b3ec1802
Stephen Whitenstall <swhitenstall@ekphrasis.co.uk>
2025-10-27 18:05:48 +0000

    Remove reporganisation files

--- wallet_utils.py
+++ wallet_utils.py
@@ -6,7 +6,7 @@ import os
 # Add src to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-from core.wallet_utils import main
+from core.wallet_utils import main  # pyright: ignore[reportMissingImports]
 
 if __name__ == "__main__":
     main()
